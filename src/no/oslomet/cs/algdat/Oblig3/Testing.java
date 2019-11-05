ada9c4dea7c89c6582ce039c46af4e67e18ae3ac
Anders <andersoyrehagen@hotmail.com>
2019-11-05 11:39:52 +0100

    Laget testingklasse

--- src/no/oslomet/cs/algdat/Oblig3/Testing.java
+++ src/no/oslomet/cs/algdat/Oblig3/Testing.java
@@ -1,5 +1,13 @@
 package no.oslomet.cs.algdat.Oblig3;
 
+import java.util.Comparator;
+
 public class Testing {
-    
+
+    // OPPGAVE 0 ////////////////////////////////////////////////
+    @org.junit.jupiter.api.Test
+    void oppgave0() {
+
+
+    }
 }
