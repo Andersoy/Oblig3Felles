ee194cd28622c1e4fae90f47213cdd5e6d039a5d
Anders <andersoyrehagen@hotmail.com>
2019-11-05 12:09:59 +0100

    Oppgave 1 og 2

--- src/no/oslomet/cs/algdat/Oblig3/ObligSBinTre.java
+++ src/no/oslomet/cs/algdat/Oblig3/ObligSBinTre.java
@@ -46,7 +46,42 @@ public class ObligSBinTre<T> implements Beholder<T>
   @Override
   public boolean leggInn(T verdi)
   {
-    throw new UnsupportedOperationException("Ikke kodet ennå!");
+
+    Objects.requireNonNull(verdi);
+
+
+    Node<T> p = rot;    // p starter i roten
+    Node<T> q = null;   // hjelpevariabel
+    int cmp = 0;
+
+    while (p != null){       // fortsetter til p er ute av treet
+
+      q = p;                                 // q er forelder til p
+      cmp = comp.compare(verdi, p.verdi);    // bruker komparatoren
+      p = cmp < 0 ? p.venstre : p.høyre;     // flytter p
+
+    }
+
+
+    // p er nå null, dvs. ute av treet, q er den siste vi passerte
+
+    p = new Node<>(verdi, q);                   // oppretter en ny node
+
+    if (q == null) {
+      rot = p;                  // p blir rotnode
+      rot.forelder = null;
+    }
+    else if (cmp < 0){
+      q.venstre = p;         // venstre barn til q
+    }
+    else{
+      q.høyre = p;                        // høyre barn til q
+    }
+
+
+    endringer++;
+    antall++;                                // én verdi mer i treet
+    return true;                             // vellykket innlegging
   }
   
   @Override
@@ -86,7 +121,34 @@ public class ObligSBinTre<T> implements Beholder<T>
   
   public int antall(T verdi)
   {
-    throw new UnsupportedOperationException("Ikke kodet ennå!");
+    if (verdi == null) {
+      return 0;
+    }
+
+    int antallAvVerdi = 0;
+    Node node = rot;
+
+
+    java.util.Deque<Node> fifo_queue = new java.util.ArrayDeque<>();
+    fifo_queue.addFirst(node);
+
+    while(fifo_queue.size() > 0) {
+      Node current = fifo_queue.removeLast();
+
+      if(current.verdi == verdi){
+        antallAvVerdi++;
+      }
+
+      if (current.venstre != null) {
+        fifo_queue.addFirst(current.venstre);
+      }
+      if (current.høyre != null) {
+        fifo_queue.addFirst(current.høyre);
+      }
+    }
+
+    return antallAvVerdi;
+
   }
   
   @Override
--- src/no/oslomet/cs/algdat/Oblig3/Testing.java
+++ src/no/oslomet/cs/algdat/Oblig3/Testing.java
@@ -10,4 +10,33 @@ public class Testing {
             ObligSBinTre<String> tre = new ObligSBinTre<>(Comparator.naturalOrder());
             System.out.println(tre.antall()); // Utskrift: 0
     }
+
+    // OPPGAVE 1 ////////////////////////////////////////////////
+    @org.junit.jupiter.api.Test
+    void oppgave1() {
+        Integer[] a = {4,7,2,9,5,10,8,1,3,6};
+        ObligSBinTre<Integer> tre = new ObligSBinTre<>(Comparator.naturalOrder());
+        for (int verdi : a) {
+            tre.leggInn(verdi);
+        }
+        System.out.println(tre.antall()); // Utskrift: 10
+    }
+
+    // OPPGAVE 2 ////////////////////////////////////////////////
+    @org.junit.jupiter.api.Test
+    void oppgave2() {
+
+        Integer[] a = {4,7,2,9,4,10,8,7,4,6};
+        ObligSBinTre<Integer> tre = new ObligSBinTre<>(Comparator.naturalOrder());
+        for(int verdi : a) {
+            tre.leggInn(verdi);
+        }
+        System.out.println(tre.antall());
+        System.out.println(tre.antall(5));
+        System.out.println(tre.antall(4));
+        System.out.println(tre.antall(7));
+        System.out.println(tre.antall(10));
+        // Utskrift: 10 ​// Utskrift: 0 ​// Utskrift: 3 ​// Utskrift: 2 ​// Utskrift: 1
+
+    }
 }
