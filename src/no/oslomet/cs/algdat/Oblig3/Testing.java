9e22cd2c991bc5c54cdc9fbf833a400e3c34b32b
Theodor Åndal <theo1-10@hotmail.com>
2019-11-05 11:53:37 +0100

    oppgave 0

--- src/no/oslomet/cs/algdat/Oblig3/Testing.java
+++ src/no/oslomet/cs/algdat/Oblig3/Testing.java
@@ -7,7 +7,7 @@ public class Testing {
     // OPPGAVE 0 ////////////////////////////////////////////////
     @org.junit.jupiter.api.Test
     void oppgave0() {
-
-
+            ObligSBinTre<String> tre = new ObligSBinTre<>(Comparator.naturalOrder());
+            System.out.println(tre.antall()); // Utskrift: 0
     }
 }
