2ebf7638f09290acea4ce246eac5af05e7deb772
CodeLord-BigT <43002939+CodeLord-BigT@users.noreply.github.com>
2019-11-06 13:51:29 +0100

    Oppgave 7 og 8 -Tobias Evju

--- src/no/oslomet/cs/algdat/Oblig3/ObligSBinTre.java
+++ src/no/oslomet/cs/algdat/Oblig3/ObligSBinTre.java
@@ -150,6 +150,125 @@ public class ObligSBinTre<T> implements Beholder<T>
     return antallAvVerdi;
 
   }
+
+  public String[] grener() {
+
+    String[] tomt = {};
+    if(tom()){
+      return tomt;
+    }
+
+    antallBladNoder = 0;
+    antallBladNoder = antallBladNoder(rot);
+    antallInnlegginger = 0;
+    grenStringArray = new String[antallBladNoder];
+    index = 0;
+
+    String[] bladNodeStringArray = utskriftAlleBladnodegrener(rot);
+
+    return bladNodeStringArray;
+  }
+  int antallBladNoder;
+  StringBuilder grenString = new StringBuilder();
+  Stakk<Node<T>> grenStakk = new TabellStakk<>();
+  String[] grenStringArray;
+  int antallInnlegginger;
+  int index;
+
+  public int antallBladNoder(Node p){
+
+    if(p != null) {
+
+      antallBladNoder(p.venstre);
+
+      if (p.venstre == null && p.høyre == null) {
+
+        antallBladNoder++;
+      }
+
+      antallBladNoder(p.høyre);
+    }
+
+    return antallBladNoder;
+  }
+
+  public String[] utskriftAlleBladnodegrener(Node p){
+
+    if(p == null) {
+      return null;
+    }
+    utskriftAlleBladnodegrener(p.venstre);
+
+    if (p.venstre == null && p.høyre == null) {
+      Node q = p;
+
+      while(p != null){
+        grenStakk.leggInn(p);
+        p = p.forelder;
+      }
+
+      while(!grenStakk.tom()){
+        if(grenString.length() == 0){
+          grenString.append("[" + grenStakk.taUt().verdi);
+        }
+        else {
+          grenString.append(", " + grenStakk.taUt().verdi);
+        }
+
+      }
+      grenString.append("]");
+      antallInnlegginger++;
+
+      for(int i = index; i < antallInnlegginger; i++) {
+        grenStringArray[i] = grenString.toString();
+      }
+      index++;
+
+      grenString = new StringBuilder();
+      p = q;
+    }
+
+    utskriftAlleBladnodegrener(p.høyre);
+
+
+    return grenStringArray;
+  }
+
+  StringBuilder bladNodeVerdier = new StringBuilder();
+
+  public String bladnodeverdier() {
+    if(antall() == 0){
+      return "[]";
+    }
+
+    bladNodeVerdier = new StringBuilder();
+    StringBuilder ferdigeBladnoder = new StringBuilder();
+    ferdigeBladnoder.append("[" +finnBladNoderInOrder(rot)+"]");
+    return ferdigeBladnoder.toString();
+  }
+
+  public String finnBladNoderInOrder(Node node) {
+
+    if (node == null) {
+      return "[]";
+    }
+
+    finnBladNoderInOrder(node.venstre);
+
+
+    if(node.venstre == null && node.høyre == null){
+
+      if(bladNodeVerdier.length() == 0){
+        bladNodeVerdier.append(node.verdi);
+      }
+      else {
+        bladNodeVerdier.append( ", " + node.verdi);
+      }
+    }
+    finnBladNoderInOrder(node.høyre);
+
+    return bladNodeVerdier.toString();
+  }
   
   @Override
   public boolean tom()
@@ -188,20 +307,87 @@ public class ObligSBinTre<T> implements Beholder<T>
   {
     throw new UnsupportedOperationException("Ikke kodet ennå!");
   }
-  
-  public String[] grener()
-  {
-    throw new UnsupportedOperationException("Ikke kodet ennå!");
-  }
-  
-  public String bladnodeverdier()
-  {
-    throw new UnsupportedOperationException("Ikke kodet ennå!");
-  }
-  
-  public String postString()
-  {
-    throw new UnsupportedOperationException("Ikke kodet ennå!");
+
+
+  public String postString() {
+
+    Node<T> p = rot;
+    if(tom()){
+      return "[]";
+    }
+
+    StringBuilder postordenString = new StringBuilder();
+    postordenString.append("[");
+    int innlegging = 0;
+
+
+    while(innlegging<antall){
+
+      while (true) {
+
+        if (p.venstre != null){
+          p = p.venstre;
+        }
+        else if (p.høyre != null){
+          p = p.høyre;
+        }
+        else{
+          break;
+        }
+      }
+
+      if(postordenString.length() == 1) {
+        postordenString.append(p.verdi);
+        innlegging++;
+      }
+      else{
+        postordenString.append(", " + p.verdi);
+        innlegging++;
+      }
+
+      while(p.forelder != null) {
+        if (p.forelder.høyre != null && p.forelder.høyre != p) {
+          p = p.forelder.høyre;
+          break;
+        } else {
+          p = p.forelder;
+          postordenString.append(", " + p.verdi);
+          innlegging++;
+        }
+      }
+    }
+
+
+//      //her er det brukt inorden-iterasjon med hjelpestack. Må gjøres om til postorden. Husk at man kan bruke foreldrepeker.
+//      Stakk<Node<T>> postStakk = new TabellStakk();
+//
+//      while(true){
+//
+//          while (p != null) {
+//
+//              postStakk.leggInn(p);
+//              p = p.venstre;
+//          }
+//
+//          if (postStakk.tom()) {
+//              break;
+//          }
+//
+//          p = postStakk.taUt();
+//
+//          if(postordenString.length() == 1){
+//              postordenString.append(p.verdi);
+//          }
+//          else {
+//              postordenString.append(", " + p.verdi);
+//          }
+//          p = p.høyre;
+//      }
+    //Til hit
+
+    postordenString.append("]");
+    return postordenString.toString();
+
   }
 
   @Override
