3ff247db1bdf5efde54ee0b8e0d38069ca8aa24a
Theodor Åndal <theo1-10@hotmail.com>
2019-11-06 15:45:11 +0100

    Oppgave 3 og 4

--- src/no/oslomet/cs/algdat/Oblig3/Testing.java
+++ src/no/oslomet/cs/algdat/Oblig3/Testing.java
@@ -39,4 +39,22 @@ public class Testing {
         // Utskrift: 10 ​// Utskrift: 0 ​// Utskrift: 3 ​// Utskrift: 2 ​// Utskrift: 1
 
     }
+
+    // OPPGAVE 3 ////////////////////////////////////////////////
+    @org.junit.jupiter.api.Test
+    void oppgave3() {
+        int[] a = {4,7,2,9,4,10,8,7,4,6,1};
+        ObligSBinTre<Integer> tre = new ObligSBinTre<>(Comparator.naturalOrder());
+        for (int verdi : a) tre.leggInn(verdi);
+        System.out.println(tre); // [1, 2, 4, 4, 4, 6, 7, 7, 8, 9, 10]
+    }
+
+    // OPPGAVE 4 ////////////////////////////////////////////////
+    @org.junit.jupiter.api.Test
+    void oppgave4() {
+        int[] a = {4,7,2,9,4,10,8,7,4,6,1};
+        ObligSBinTre<Integer> tre = new ObligSBinTre<>(Comparator.naturalOrder());
+        for (int verdi : a) tre.leggInn(verdi);
+        System.out.println(tre.omvendtString()); // [10, 9, 8, 7, 7, 6, 4, 4, 4, 2, 1]
+    }
 }
